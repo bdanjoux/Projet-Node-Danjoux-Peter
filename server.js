ae63b7b386e8f159cdd03ff7dc68c60e602f22a9
Benjamin Danjoux <benjamin.danjoux@gmail.com>
2019-06-06 16:24:28 +0200

    can now charge a brain to a bot

--- server.js
+++ server.js
@@ -102,6 +102,12 @@ app.get('/allBotNames', function(req, res) {
     res.send(JSON.stringify(manager.getAllBotNames()));
 });
 
+// admin page
+app.get('/allConfigFiles', function(req, res) {
+    res.send(JSON.stringify(manager.getAllAvailableConfigFiles()));
+});
+
+
 app.get('/allSalonNames', function(req, res){
     res.send(JSON.stringify(manager.getAllSalonNames()));
 });
@@ -157,6 +163,12 @@ app.post('/createBot', function(req, res, next){
     manager.addBotToSalon(req.body.botName,8081);
     console.log(manager.getAllBotNames());
     res.render('pages/about');
+});
+
+app.post('/botInSalonLoadFile', function(req, res, next){
+    console.log("");
+    manager.botInSalonLoadFile(req.body.name,parseInt(req.body.port),req.body.configFile);//TODO: récupérer les trucs du fichier botCreate
+    res.render('pages/botAdmin.ejs');
 });
 
 
